34ebc234683912c53cc70739b0224548eaf5e21f
just-maik <maik.jonietz@googlemail.com>
2023-06-28 08:54:58 +0000

    update deps and add some packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,40 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    extends: [
-        'nuxt-seo-kit'
-    ],
-    modules: [
-        '@nuxtjs/tailwindcss',
-        '@nuxtjs/device',
-        '@nuxt/image',
-        '@vueuse/nuxt',
-        'nuxt-umami',
-        'nuxt-svgo'
-    ],
-    app: {
-        head: {
-            titleTemplate: '%pageTitle %titleSeparator %siteName'
-        }
-    },
-    unhead: {
-        ogTitleTemplate: "%s | Maik's Starter",
-    },
-    runtimeConfig: {
-        public: {
-            siteUrl: process.env.NUXT_PUBLIC_SITE_URL || 'https://localhoost:3000',
-            siteName: 'Maiks Starter',
-            siteDescription: 'Welcome to maiks starter!',
-            language: 'en',
-            titleSeparator: '|'
-        }
-    },
-    umami: {
-        enable: false,
-        autoTrack: true,
-        doNotTrack: false,
-        cache: false,
-        domains: 'mywebsite.com,mywebsite2.com',
-        websiteId: 'your-website-id',
-        scriptUrl: 'https://path.to.umami.js',
-      }
-})
+	extends: ["nuxt-seo-kit"],
+	modules: [
+		"@nuxtjs/tailwindcss",
+		"@nuxtjs/device",
+		"@vueuse/nuxt",
+		"nuxt-svgo",
+	],
+
+	app: {
+		head: {
+			titleTemplate: "%pageTitle %titleSeparator %siteName",
+		},
+	},
+	runtimeConfig: {
+		public: {
+			siteUrl:
+				process.env.NUXT_PUBLIC_SITE_URL || "https://localhoost:3000",
+			siteName: "Maiks Starter",
+			siteDescription: "Welcome to maiks starter!",
+			language: "en",
+			titleSeparator: "|",
+		},
+	},
+	telemetry: false,
+	devtools: true,
+});
